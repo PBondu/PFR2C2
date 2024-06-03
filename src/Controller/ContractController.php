2a34f1565e9b397b1dded22c4e7a79482c6100e6
PBondu <paul.bondu@proton.me>
2024-06-03 17:41:59 +0200

    ajout recherche billing à partir contrat

--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -10,16 +10,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BillingRepository;
+
 
 #[Route('/contract')]
 class ContractController extends AbstractController
 {
+    // Route en annotations
     #[Route('/', name: 'app_contract_index', methods: ['GET'])]
-    public function index(ContractRepository $contractRepository): Response
+    public function index(ContractRepository $contractRepository, Request $request, BillingRepository $billingRepository): Response
     {
+      // Init et permet de ne pas lancer de valeurs dans le twig si l'objet n'est pas trouvé
+      $sqlData = null;
+      // Condition pour vérifier que la méthode HTTP est POST
+      if ($request->isMethod('POST')) {
+          // Récupère l'input utilisateur et le stocke dans une variable
+          $sqlId = $request->request->get('sql_id');
+          // Cherche l'objet concerné par l'id saisi ci-dessus
+          $contractArray = $contractRepository->findBy(array('id' => (int)$sqlId));
+          // Si l'objet est trouvé
+          if ($contractArray) {
+              // Fait correspondre un objet de billing avec la clef primaire de l'objet trouvé
+              $sqlData = $billingRepository->findBy(['Contract_id' => $contractArray]);
+          }
+      }
+        // Render des variables pour afficahge dans le twig
         return $this->render('contract/index.html.twig', [
-            'contracts' => $contractRepository->findAll(),
-        ]);
+          'contracts' => $contractRepository->findAll(),// Affichage de base
+          'sqlData' => $sqlData, // Affichage de la recherche utilisateur
+      ]);
     }
 
     #[Route('/new', name: 'app_contract_new', methods: ['GET', 'POST'])]
