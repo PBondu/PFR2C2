55ff9ded09a1ba41d96aa80089f1910beabb3647
PBondu <paul.bondu@proton.me>
2024-06-04 16:18:19 +0200

    unpayed rent added

--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -16,85 +16,139 @@ use App\Repository\BillingRepository;
 #[Route('/contract')]
 class ContractController extends AbstractController
 {
-    // Route en annotations
-    #[Route('/', name: 'app_contract_index', methods: ['GET'])]
-    public function index(ContractRepository $contractRepository, Request $request, BillingRepository $billingRepository): Response
-    {
-      // Init et permet de ne pas lancer de valeurs dans le twig si l'objet n'est pas trouvé
-      $sqlData = null;
-      // Condition pour vérifier que la méthode HTTP est POST
-      if ($request->isMethod('POST')) {
-          // Récupère l'input utilisateur et le stocke dans une variable
-          $sqlId = $request->request->get('sql_id');
-          // Cherche l'objet concerné par l'id saisi ci-dessus
-          $contractArray = $contractRepository->findBy(array('id' => (int)$sqlId));
-          // Si l'objet est trouvé
-          if ($contractArray) {
-              // Fait correspondre un objet de billing avec la clef primaire de l'objet trouvé
-              $sqlData = $billingRepository->findBy(['Contract_id' => $contractArray]);
-          }
+  // Route en annotations
+  #[Route('/', name: 'app_contract_index', methods: ['GET'])]
+  public function index(ContractRepository $contractRepository, Request $request, BillingRepository $billingRepository): Response
+  {
+    /*
+        RECHERCHE BILLING A PARTIR ID CONTRAT
+      */
+    // Init et permet de ne pas lancer de valeurs dans le twig si l'objet n'est pas trouvé
+    $sqlData = null;
+    // Condition pour vérifier que la méthode HTTP est POST
+    if ($request->isMethod('POST')) {
+      // Récupère l'input utilisateur et le stocke dans une variable
+      $id_from_input = $request->request->get('sql_id');
+      // Cherche l'objet concerné par l'id saisi ci-dessus
+      $contractArrayBill = $contractRepository->findBy(array('id' => (int)$id_from_input));
+      // Si l'objet est trouvé
+      if ($contractArrayBill) {
+        // Fait correspondre un objet de billing avec la clef primaire de l'objet trouvé
+        $sqlData = $billingRepository->findBy(['Contract_id' => $contractArrayBill]);
       }
-        // Render des variables pour afficahge dans le twig
-        return $this->render('contract/index.html.twig', [
-          'contracts' => $contractRepository->findAll(),// Affichage de base
-          'sqlData' => $sqlData, // Affichage de la recherche utilisateur
-      ]);
     }
+    /*
+        FIN RECHERCHE BILLING
+      */
 
-    #[Route('/new', name: 'app_contract_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $contract = new Contract();
-        $form = $this->createForm(ContractType::class, $contract);
-        $form->handleRequest($request);
+    /*
+        RECHERCHE CONTRACT A PARTIR ID CONTRAT
+      */
+    // Init et permet de ne pas lancer de valeurs dans le twig si l'objet n'est pas trouvé
+    $contractSelected = null;
+    // Condition pour vérifier que la méthode HTTP est POST
+    if ($request->isMethod('POST')) {
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($contract);
-            $entityManager->flush();
+      // Récupère l'input utilisateur et le stocke dans une variable
+      $id_from_input = $request->request->get('contract_id');
+      // Cherche l'objet concerné par l'id saisi ci-dessus
+      $contractArray = $contractRepository->findBy(array('id' => (int)$id_from_input));
+      // Si l'objet est trouvé
+      if ($contractArray) {
+        // Fait correspondre un objet de billing avec la clef primaire de l'objet trouvé
+        $contractSelected = $contractArray;
+      }
+    }
+    /*
+        FIN RECHERCHE CONTRACT
+      */
 
-            return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
+    /*
+        CONTRATS IMPAYES
+      */
+    $unpayed = $request->request->get('unpayed');
+    $unpayedContracts = [];
+
+    if ($unpayed == 'show') {
+      
+      foreach ($contractRepository->findAll() as $cont) {
+        $compare = $billingRepository->findBy(['Contract_id' => $cont]);
+
+        if ($compare == []) {
+          $unpayedContracts[] = $cont;
         }
+      }  
+    }
+    else{
+      $unpayedContracts = [];
+    }
+    /*
+        FIN CONTRATS IMPAYES
+      */
 
-        return $this->render('contract/new.html.twig', [
-            'contract' => $contract,
-            'form' => $form,
-        ]);
+    // Render des variables pour afficahge dans le twig
+    return $this->render('contract/index.html.twig', [
+      'contracts' => $contractRepository->findAll(), // Affichage de la liste complète
+      'sqlData' => $sqlData, // Affichage de la recherche utilisateur
+      'contractSelected' => $contractSelected,
+      'unpayedContracts' => $unpayedContracts,
+    ]);
+  }
+
+  #[Route('/new', name: 'app_contract_new', methods: ['GET', 'POST'])]
+  public function new(Request $request, EntityManagerInterface $entityManager): Response
+  {
+    $contract = new Contract();
+    $form = $this->createForm(ContractType::class, $contract);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->persist($contract);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}', name: 'app_contract_show', methods: ['GET'])]
-    public function show(Contract $contract): Response
-    {
-        return $this->render('contract/show.html.twig', [
-            'contract' => $contract,
-        ]);
+    return $this->render('contract/new.html.twig', [
+      'contract' => $contract,
+      'form' => $form,
+    ]);
+  }
+
+  #[Route('/{id}', name: 'app_contract_show', methods: ['GET'])]
+  public function show(Contract $contract): Response
+  {
+    return $this->render('contract/show.html.twig', [
+      'contract' => $contract,
+    ]);
+  }
+
+  #[Route('/{id}/edit', name: 'app_contract_edit', methods: ['GET', 'POST'])]
+  public function edit(Request $request, Contract $contract, EntityManagerInterface $entityManager): Response
+  {
+    $form = $this->createForm(ContractType::class, $contract);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}/edit', name: 'app_contract_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Contract $contract, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ContractType::class, $contract);
-        $form->handleRequest($request);
+    return $this->render('contract/edit.html.twig', [
+      'contract' => $contract,
+      'form' => $form,
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('contract/edit.html.twig', [
-            'contract' => $contract,
-            'form' => $form,
-        ]);
+  #[Route('/{id}', name: 'app_contract_delete', methods: ['POST'])]
+  public function delete(Request $request, Contract $contract, EntityManagerInterface $entityManager): Response
+  {
+    if ($this->isCsrfTokenValid('delete' . $contract->getId(), $request->getPayload()->get('_token'))) {
+      $entityManager->remove($contract);
+      $entityManager->flush();
     }
 
-    #[Route('/{id}', name: 'app_contract_delete', methods: ['POST'])]
-    public function delete(Request $request, Contract $contract, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$contract->getId(), $request->getPayload()->get('_token'))) {
-            $entityManager->remove($contract);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
-    }
+    return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
+  }
 }
